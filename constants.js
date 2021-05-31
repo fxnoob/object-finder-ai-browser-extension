f8d9130e3a44d189a503e31e4312237315e3cf6a
fxnoob <fxnoob71@gmail.com>
2021-05-31 09:51:33 +0530

    update endpoint

--- constants.js
+++ constants.js
@@ -4,7 +4,7 @@ const constants = {
     appName: "Object Finder AI",
     // put extension key here if required which would only be used in development mode
     key:"-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAniodbRULqdbI5fqDW3p9\n1hHU2WGZzNvf0/spfO416davV7jMvUblpbiZhpoBgUVYGWnbJd0V88tDo3YiI96L\nSAZJ+ZB5QgGxcIrsMT3NpllvoqThNKOo/FK/z+OtUYtDaADVmtxeGe+cDD75n/NO\nrhAAQP7342zA0yKMulNA9bwsGxzLSb0xWUNSk1Rr31VE6egraVZATwSi+ZaG5ZRf\nnKBPhq3Lups3ZmuWYEV+u801oLCwu8vqc9kLPwsmhPGAMau7giyPkR8biWaqpYm3\nFnB8nuNdQv7UXd1OYxHPGUxbJNZlN3m1sLqebKR10lVODZdJdCRkQmLUmiE/j8P7\ngQIDAQAB\n-----END PUBLIC KEY-----",
-    endpoint: process.env.mode != "production" ? "https://imagetext.xyz": "http://localhost:3000",
+    endpoint: process.env.mode === "production" ? "https://imagetext.xyz": "http://localhost:3000",
   },
   contentScript: {
     mountId: generateGuid(),
